f932e82d882c3c505642000eec55de4beec28128
StoneLink Developer <developer@stonelink.local>
2026-06-30 11:19:31 -0300

    Update neo4j_conn.py

--- neo4j_conn.py
+++ neo4j_conn.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
-NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
+NEO4J_USER = os.getenv("NEO4J_USER") or os.getenv("NEO4J_USERNAME", "neo4j")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "stonelink123")
 
 driver = GraphDatabase.driver(
